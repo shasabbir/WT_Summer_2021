412852d445e65a560858ee233e9f3d38316f8701
Sabbir <shasabbir234@gmail.com>
2021-05-30 16:00:20 +0600

    Your message about the commit

---
+++ Lab-task1/Lab-task1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Form</title>
+</head>
+<body>
+<h2>Registration Form</h2>
+<form>
+	Full Name: <input type="text" name="name"><br>
+	Email: <input type="email" name="email"><br>
+	Password: <input type="hidden" name="pass"><br>
+	Comment: <input type="text" name="comment"><br>
+	Choose gender ->
+	male<input type="radio" name="gender">
+	female<input type="radio" name="gender"><br>
+	other<input type="radio" name="gender"><br>
+	Please select hobby ->
+	singing<input type="checkbox" name="hobby">
+	dancing<input type="checkbox" name="hobby"><br>
+	reading<input type="checkbox" name="hobby"><br>
+	Please choose a file -><input type="file"><br>
+	<input type="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
