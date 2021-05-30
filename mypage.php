16b71dde2942f0c32c381fc2431589ba7ec0ce5d
Sabbir Hoaasin <58880363+shasabbir@users.noreply.github.com>
2021-05-30 19:51:49 +0600

    Add files via upload

---
+++ mypage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My home page</title>
+</head>
+<body>
+<h2>My home page</h2>
+<h4>my profile</h4>
+<form>
+	first name: <input type="text" name="fn"><br>
+	last name: <input type="text" name="ln"><br>
+	Choose gender:<br>
+	male<input type="radio" name="gender">
+	female<input type="radio" name="gender"><br>
+	<input type="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
