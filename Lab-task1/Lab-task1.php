9303476eeddbd5182f88d7e7551d290368899300
Sabbir <shasabbir234@gmail.com>
2021-05-30 19:18:22 +0600

    Your message about the commit

--- Lab-task1/Lab-task1.php
+++ Lab-task1/Lab-task1.php
@@ -6,19 +6,45 @@
 <body>
 <h2>Registration Form</h2>
 <form>
-	Full Name: <input type="text" name="name"><br>
-	Email: <input type="email" name="email"><br>
-	Password: <input type="hidden" name="pass"><br>
-	Comment: <input type="text" name="comment"><br>
-	Choose gender ->
-	male<input type="radio" name="gender">
-	female<input type="radio" name="gender"><br>
+	<table style="width:40%">
+  <tr>
+    <th> </td>
+    <th> </td>
+  </tr>
+  <tr>
+    <td>Full Name: </td>
+    <td><input type="text" name="name"><br></td>
+  </tr>
+  <tr>
+    <td>Email: </td>
+    <td><input type="email" name="email"><br></td>
+  </tr>
+  <tr>
+    <td>Password: </td>
+    <td><input type="hidden" name="pass"><br></td>
+  </tr>
+  <tr>
+    <td>Comment: </td>
+    <td><textarea name="comment" rows="4" cols="50"></textarea><br></td>
+  </tr>
+  <tr>
+    <td>Choose gender</td>
+    <td>male<input type="radio" name="gender">
+	female<input type="radio" name="gender">
 	other<input type="radio" name="gender"><br>
-	Please select hobby ->
-	singing<input type="checkbox" name="hobby">
-	dancing<input type="checkbox" name="hobby"><br>
-	reading<input type="checkbox" name="hobby"><br>
-	Please choose a file -><input type="file"><br>
+  </tr>
+  <tr>
+    <td>Please select hobby</td>
+    <td>singing<input type="checkbox" name="hobby">
+	dancing<input type="checkbox" name="hobby">
+	reading<input type="checkbox" name="hobby"></td>
+  </tr>
+  <tr>
+    <td>Please choose a file</td>
+    <td><input type="file"></td>
+  </tr>
+</table>
+
 	<input type="submit" name="submit" value="submit">
 </form>
 </body>
